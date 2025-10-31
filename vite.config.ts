2fb3b1d1a30543fab9eb0862f2bd0df3571b2c88
hepq <2022e16@stu.gifu-nct.ac.jp>
2025-11-01 06:52:31 +0900

    fix vite config 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = "/logic_circuit_maker/";
+//const repoName = "/logic_circuit_maker/";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: repoName,
+  base: "./",
 });
